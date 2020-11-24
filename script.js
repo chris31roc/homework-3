0616c03e1c5a248658e99f3089913a7e80ec4fcd
chris31roc <chris31roc@yahoo.com>
2020-11-24 14:01:10 -0700

    created variables for numbers, special characters, upper case characters and lower case characters and created a prompt from the generate button

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Checking to see if I'm connected to my application
+console.log("App Connected")
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Linking the element IDs  for the generate and password button
+var generateBtn = document.getElementById("generate");
+var passwordText = document.getElementById("password");
 
-  passwordText.value = password;
+generateBtn.addEventListener("click", function(event) {
+event.preventDefault();
+generatePassword()
+});
+
+//Assigning the different types of characters to be used for the generator
+var numbers = "1234567890" .split("")
+var specialChar = "!@#$%^&*()_+?<>}{[]`~" .split("")
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("")
+var lowerChar = "abcdefghijklmnopqrstuvwxyz" .split("")
+
+//Checking to make sure they are logged correctly
+console.log(numbers)
+console.log(specialChar)
+console.log(upperChar)
+console.log(lowerChar)
+
+//Prompts and instructions for the user to start 
+function generatePassword() {
+  var passLength = prompt(
+    "How many characters would you like your password to be?\nPlease choose a number between 8 and 128 characters.\n\nThen click OK!"
+    );
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
